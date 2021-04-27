3cc80ba2cc621dc1a29cb654bc696837bafa82c6
Teo Martin Toledo <49320625+teootoledo@users.noreply.github.com>
2021-04-27 11:46:02 -0300

    Avances importantes con el grid

--- frontend/src/components/Simulator/index.js
+++ frontend/src/components/Simulator/index.js
@@ -18,6 +18,14 @@ import Student1 from "../../assets/students/Estudiante1.svg";
 export default function Simulator() {
   return (
     <div>
+      <div className="menu">
+        <h2>Why Is the Risk of Coronavirus Transmission so High Indoors?</h2>
+        <p className="header-text">
+          Whenever people gather in closed spaces, the infection risk climbs.
+          Our interactive tool shows how the coronavirus spreads. Find out how
+          safe your environment is.
+        </p>
+      </div>
       <MainContainer />
     </div>
   );
@@ -26,19 +34,42 @@ export default function Simulator() {
 function MainContainer() {
   return (
     <>
-      <div className="menu">
-        <h3>Why Is the Risk of Coronavirus Transmission so High Indoors?</h3>
-        <p className="header-text">
-          Whenever people gather in closed spaces, the infection risk climbs.
-          Our interactive tool shows how the coronavirus spreads. Find out how
-          safe your environment is.
-        </p>
-      </div>
-      <div className="wrapper">
-        <Room />
-        <Menu />
+      <div>
+        <div className="wrapper">
+          <Room />
+          <Menu />
+        </div>
       </div>
     </>
+  );
+}
+
+function Room() {
+  return (
+    <div className="room-container">
+      <div className="room">
+        <div className="room-soil soil-shadow">
+          <div className="room-grid">
+            <div className="iso-element">Hi</div>
+            <div className="iso-element">I'm</div>
+            <div className="iso-element">an</div>
+            <div className="iso-element">
+              <div className="student-box">
+                <div className="student-wrapper">
+                  <img src={Student1} alt="Estudiante" />
+                </div>
+              </div>
+            </div>
+            <div className="iso-element">Hi</div>
+            <div className="iso-element">I'm</div>
+            <div className="iso-element">an</div>
+            <div className="iso-element">element</div>
+          </div>
+          <div className="left-wall wall"></div>
+          <div className="right-wall wall"></div>
+        </div>
+      </div>
+    </div>
   );
 }
 
@@ -47,27 +78,6 @@ function Menu() {
     <div className="room-parameters">
       <div className="parameters-header">
         <h5 className="parameters-title">Room parameters</h5>
-      </div>
-    </div>
-  );
-}
-
-function Room() {
-  return (
-    <div className="room">
-      <div className="room-soil soil-shadow">
-        <div className="room-grid">
-          <div className="iso-element">Hi</div>
-          <div className="iso-element">I'm</div>
-          <div className="iso-element">an</div>
-          <div className="iso-element">element</div>
-          <div className="iso-element">Hi</div>
-          <div className="iso-element">I'm</div>
-          <div className="iso-element">an</div>
-          <div className="iso-element">element</div>
-        </div>
-        <div className="left-wall wall"></div>
-        <div className="right-wall wall"></div>
       </div>
     </div>
   );
