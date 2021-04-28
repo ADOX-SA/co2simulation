2c27eaa12c555f1a49f929ceac7413a1968dd41d
Teo Martin Toledo <49320625+teootoledo@users.noreply.github.com>
2021-04-28 12:48:17 -0300

    Habitación completada de assets

--- frontend/src/components/Simulator/index.js
+++ frontend/src/components/Simulator/index.js
@@ -12,6 +12,10 @@ import Student4 from "../../assets/students/Estudiante4.svg";
 import Student3 from "../../assets/students/Estudiante3.svg";
 import Student2 from "../../assets/students/Estudiante2.svg";
 import Student1 from "../../assets/students/Estudiante1.svg";
+import Student0 from "../../assets/students/Estudiante0.svg";
+import Profesora from "../../assets/people/Teacher.svg";
+import EscritorioProfesora from "../../assets/furniture/TeacherDesk.svg";
+import Biblioteca from "../../assets/furniture/Bookshelf.svg";
 
 // ========================================
 
@@ -49,65 +53,82 @@ function Room() {
     <div className="room-container">
       <div className="room">
         <div className="room-floor floor-shadow">
-          <div className="room-grid">
-            <div className="iso-element">Hi</div>
-            <div className="iso-element">I'm</div>
-            <div className="iso-element">an</div>
-            <div className="iso-element">
-              <div className="student-box">
-                <div className="student-wrapper">
-                  <img src={Student1} alt="Estudiante" />
-                </div>
-              </div>
-            </div>
-            <div className="iso-element">Hi</div>
-            <div className="iso-element">I'm</div>
-            <div className="iso-element">an</div>
-            <div className="iso-element">
-              <div className="student-box">
-                <div className="student-wrapper">
-                  <img src={Student2} alt="Estudiante" />
-                </div>
-              </div>
-            </div>
-            <div className="iso-element">
-              <div className="student-box">
-                <div className="student-wrapper">
-                  <img src={Student2} alt="Estudiante" />
-                </div>
-              </div>
-            </div>
-            <div className="iso-element">
-              <div className="student-box">
-                <div className="student-wrapper">
-                  <img src={Student2} alt="Estudiante" />
-                </div>
-              </div>
-            </div>
-            <div className="iso-element">
-              <div className="student-box">
-                <div className="student-wrapper">
-                  <img src={Student2} alt="Estudiante" />
-                </div>
-              </div>
-            </div>
-            <div className="iso-element">
-              <div className="student-box">
-                <div className="student-wrapper">
-                  <img src={Student2} alt="Estudiante" />
-                </div>
-              </div>
-            </div>
-            <div className="iso-element">
-              <div className="student-box">
-                <div className="student-wrapper">
-                  <img src={Student2} alt="Estudiante" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="left-wall wall"></div>
-          <div className="right-wall wall"></div>
+          <Grid />
+          <Walls />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Grid() {
+  return (
+    <div className="room-grid">
+      <Shelf />
+      <StudentDesk />
+      <StudentDesk />
+      <StudentDesk />
+      <Teacher />
+      <StudentDesk />
+      <StudentDesk />
+      <StudentDesk />
+      <StudentDesk />
+      <StudentDesk />
+      <StudentDesk />
+      <TeacherDesk />
+      <StudentDesk />
+      <StudentDesk />
+      <StudentDesk />
+    </div>
+  );
+}
+
+function Walls() {
+  return (
+    <>
+      <div className="left-wall wall"></div>
+      <div className="right-wall wall"></div>
+    </>
+  );
+}
+
+function Shelf() {
+  return (
+    <div className="iso-element">
+      <div className="shelf-wrapper">
+        <img src={Biblioteca} alt="Biblioteca" />
+      </div>
+    </div>
+  );
+}
+
+function StudentDesk() {
+  return (
+    <div className="iso-element">
+      <div className="iso-box">
+        <div className="student-wrapper">
+          <img src={Student0} alt="Estudiante" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Teacher() {
+  return (
+    <div className="iso-element doble">
+      <div className="people-wrapper">
+        <img src={Profesora} alt="Profesora" />
+      </div>
+    </div>
+  );
+}
+function TeacherDesk() {
+  return (
+    <div className="iso-element">
+      <div className="iso-box">
+        <div className="desk-wrapper">
+          <img src={EscritorioProfesora} alt="Escritorio de profesora" />
         </div>
       </div>
     </div>
