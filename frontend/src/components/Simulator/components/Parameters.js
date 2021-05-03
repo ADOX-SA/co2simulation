802e80cdd7b69665f47be425495614a56abc71c1
Teo Martin Toledo <49320625+teootoledo@users.noreply.github.com>
2021-05-03 11:45:59 -0300

    Problemas con slider

--- frontend/src/components/Simulator/components/Parameters.js
+++ frontend/src/components/Simulator/components/Parameters.js
@@ -1,39 +1,41 @@
 import React, { useState } from "react";
 
 export default function Parameters() {
+  const [peopleSlider1, setPeopleSlider1] = useState({
+    value: 1,
+    min: 1,
+    max: 12,
+    label: "people",
+  });
+
   return (
     <div className="room-parameters">
       <div className="parameters-header">
         <h5 className="parameters-title">Room parameters</h5>
       </div>
       <div className="controllers-div">
-        <Slider />
-        <Slider />
+        <Slider slider={peopleSlider1} setSlider={setPeopleSlider1} />
       </div>
     </div>
   );
 }
 
-function Slider() {
-  const [valor, setValor] = useState({ value: 0 });
-
-  const slideValue = document.querySelector("span");
-
+function Slider({ slider, setSlider }) {
   const handleOnChange = (e) => {
-    setValor({ value: e.target.value });
+    setSlider({ value: e.target.value });
   };
 
   return (
     <div className="range shadow">
       <div className="slider-value">
-        <span>{valor.value}</span>
+        <span>{slider.value}</span>
       </div>
       <div className="field">
         <input
           type="range"
-          min="0"
-          max="12"
-          value={valor.value}
+          min={slider.min}
+          max={slider.max}
+          value={slider.value}
           onChange={handleOnChange}
         />
       </div>
