bc662b108b3c44cc7242bca2ef557b491ada9669
Teo Martin Toledo <49320625+teootoledo@users.noreply.github.com>
2021-05-03 12:11:43 -0300

    Slider funcionando

--- frontend/src/components/Simulator/components/Parameters.js
+++ frontend/src/components/Simulator/components/Parameters.js
@@ -1,41 +1,42 @@
 import React, { useState } from "react";
 
 export default function Parameters() {
-  const [peopleSlider1, setPeopleSlider1] = useState({
-    value: 1,
-    min: 1,
-    max: 12,
-    label: "people",
-  });
-
   return (
     <div className="room-parameters">
       <div className="parameters-header">
         <h5 className="parameters-title">Room parameters</h5>
       </div>
       <div className="controllers-div">
-        <Slider slider={peopleSlider1} setSlider={setPeopleSlider1} />
+        D
+        <Slider min={1} max={12} label="people" />
+        <Slider min={1} max={12} label="people" unit="hr" />
       </div>
     </div>
   );
 }
 
-function Slider({ slider, setSlider }) {
+function Slider({ min, max, label, unit }) {
+  const [value, setValue] = useState({ value: 1 });
+
   const handleOnChange = (e) => {
-    setSlider({ value: e.target.value });
+    setValue({ value: e.target.value });
   };
 
   return (
     <div className="range shadow">
-      <div className="slider-value">
-        <span>{slider.value}</span>
+      <div className="slider-info">
+        <span className="slider-value">
+          {value.value}
+          {unit}
+        </span>
+        <span className="slider-label">{label}</span>
       </div>
       <div className="field">
         <input
           type="range"
-          min={slider.min}
-          max={slider.max}
-          value={slider.value}
+          min={min}
+          max={max}
+          value={value.value}
           onChange={handleOnChange}
         />
       </div>
