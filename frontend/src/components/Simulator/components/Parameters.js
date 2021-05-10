5339787f2bfe182b50175803b7dbb480bcd7cc0b
Teo Martin Toledo <49320625+teootoledo@users.noreply.github.com>
2021-05-10 12:34:47 -0300

    Modificación de alcance para primer entrega

--- frontend/src/components/Simulator/components/Parameters.js
+++ frontend/src/components/Simulator/components/Parameters.js
@@ -20,7 +20,7 @@ export default function Parameters() {
         <h5 className="parameters-title">Parámetros del ambiente</h5>
       </div>
       <div className="controllers-div">
-        <PeopleSlider min={1} max={12} label="personas" />
+        {/* <PeopleSlider min={1} max={12} label="personas" /> */}
         <DurationSlider min={1} max={9} label="duración" unit="hr" />
         <MaskSelector />
         <VentilationSelector />
@@ -66,24 +66,27 @@ function DurationSlider({ min, max, label, unit }) {
   };
 
   return (
-    <div className="range shadow">
-      <div className="slider-info">
-        <span className="slider-value">
-          {room.duration}
-          {unit}
-        </span>
-        <span className="slider-label">{label}</span>
+    <>
+      <h5 className="slider-title">Duración de permanencia en ambiente:</h5>
+      <div className="range shadow">
+        <div className="slider-info">
+          <span className="slider-value">
+            {room.duration}
+            {unit}
+          </span>
+          <span className="slider-label">{label}</span>
+        </div>
+        <div className="field">
+          <input
+            type="range"
+            min={min}
+            max={max}
+            value={room.duration}
+            onChange={handleOnChange}
+          />
+        </div>
       </div>
-      <div className="field">
-        <input
-          type="range"
-          min={min}
-          max={max}
-          value={room.duration}
-          onChange={handleOnChange}
-        />
-      </div>
-    </div>
+    </>
   );
 }
 
