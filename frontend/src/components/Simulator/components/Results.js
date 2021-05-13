17c8d55b866ced2a7ba2dde06a58f8f01c7160a2
Teo Martin Toledo <teootoledo@gmail.com>
2021-05-13 11:00:13 -0300

    Muestro ppm

--- frontend/src/components/Simulator/components/Results.js
+++ frontend/src/components/Simulator/components/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRoom } from "../../../context/room-context";
 
 // Imported assets
 import sensor from "../../../assets/sensor/Sensor.svg";
@@ -8,6 +9,8 @@ import sensorAlto from "../../../assets/sensor/SensorAlto.svg";
 import sensorPeligro from "../../../assets/sensor/SensorPeligro.svg";
 
 export default function Results() {
+  const { room } = useRoom();
+
   return (
     <div className="results-card">
       <div className="results-header">
@@ -15,7 +18,20 @@ export default function Results() {
       </div>
       <div className="results-wrapper">
         <div>
-          <h5>Resultados</h5>
+          <h5>
+            Total CO<sub>2</sub> presente en el ambiente (partes por millón)
+          </h5>
+          <h1
+            className={
+              room.totalCO2Ambiente < 800
+                ? "ppmOK"
+                : room.totalCO2Ambiente >= 800 && room.totalCO2Ambiente < 1400
+                ? "ppmAlto"
+                : "ppmPeligro"
+            }
+          >
+            {parseInt(room.totalCO2Ambiente)} ppm
+          </h1>
         </div>
         <div className="sensor-div">
           <img className="sensor-img" src={sensorAlto} alt="Sensor" />
