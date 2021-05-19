968bc6ef2bd380725d7ff879f8230862ad8621dd
Teo Martin Toledo <teootoledo@gmail.com>
2021-05-19 13:10:25 -0300

    Agrego informacion

--- src/components/Information/index.js
+++ src/components/Information/index.js
@@ -1,9 +1,45 @@
 import React from "react";
+import { infoList } from "./info-list";
 
 export default function Information() {
   return (
     <div className="info-container">
       <h1>Información</h1>
+      <Post post={infoList[0]} />
+      <Post post={infoList[1]} />
+      <Post post={infoList[2]} />
+    </div>
+  );
+}
+
+function Post(props) {
+  return (
+    <div className="post-container">
+      <PostHeader subtitle={props.post.subtitle} title={props.post.title} />
+      <PostContent content={props.post} />
+    </div>
+  );
+}
+
+function PostHeader(props) {
+  return (
+    <div className="post-header">
+      <h2 className="post-subtitle">{props.subtitle}</h2>
+      <h1 className="post-title">{props.title}</h1>
+    </div>
+  );
+}
+
+function PostContent(props) {
+  return (
+    <div className="post-content">
+      {props.content.texts.map((text) => {
+        return (
+          <p className="post-content-text" key={text}>
+            {text}
+          </p>
+        );
+      })}
     </div>
   );
 }
